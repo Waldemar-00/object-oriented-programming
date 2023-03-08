11295b2ea3163bb966d15e314993eee0208b4fe6
uladzimir <vova108vova@gmail.com>
2023-03-08 16:53:43 +0300

    slider with class

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,7 +1,41 @@
 export default class Slider {
     constructor(page, buttons) {
         this.page = document.querySelector(page);
-        this.sliders = this.page.children;
+        this.slides = Array.from(this.page.children);
         this.buttons = document.querySelectorAll(buttons);
+        this.index = 1;
+    }
+    showSlides(index) {
+        if (index > this.slides.length) {
+            this.index = 1;
+        }
+        if (index < 1) {
+            this.index = this.slides.length;
+        }
+        this.slides.forEach(slide => {
+            slide.style.display = 'none';
+        });
+        this.slides[this.index - 1].style.display = 'block';
+    }
+
+    plusSlide(step) {
+        this.showSlides(this.index += step);
+    }
+    minusSlide(step) {
+    this.showSlides(this.index -= step);
+    }
+
+    render() {
+        this.buttons.forEach(btn => {
+            btn.addEventListener('click', () => {
+                this.plusSlide(1);
+            });
+            btn.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.index = 1;
+                this.showSlides(this.index);
+            });
+        });
+        this.showSlides(this.index);
     }
 }
