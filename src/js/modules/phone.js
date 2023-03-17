44e33f54c264446c6babcb4b2d5fe24e04155dd6
uladzimir <vova108vova@gmail.com>
2023-03-17 21:18:29 +0300

    finished mask of phone

--- src/js/modules/phone.js
+++ src/js/modules/phone.js
@@ -9,6 +9,12 @@ export default function phone(inputTel) {
         if (!inputValueNumbers) {
             return (e.target.value = '');
         }
+        if (e.target.selectionStart !== e.target.value.length) {
+            if (e.data && /\D/g.test(e.data)) {
+                e.target.value = inputValueNumbers;
+            }
+            return;
+        }
         if (['7', '8', '9'].indexOf(inputValueNumbers[0]) > (- 1)) {
             if (inputValueNumbers[0] === '9') {
                 inputValueNumbers = '7' + inputValueNumbers;
@@ -33,7 +39,25 @@ export default function phone(inputTel) {
         e.target.value = formattedInputValue;
         console.log(formattedInputValue);
     }
+    function onPhoneKeyDown(e) {
+        console.log(e.keyCode, '--', e.target.value);
+        console.log(e.target.value.length);
+        if (e.keyCode === 8 && getInputValueNumbers(e.target).length === 1) {
+            e.target.value = '';
+        }
+    }
+    function onPhonePaste(e) {
+        const paste = e.clipboardData || window.clipboardData;
+        if (paste) {
+            const pastedText = paste.getData('text');
+            if (/\D/g.test(pastedText)) {
+                e.target.value = getInputValueNumbers(e.target);
+            }
+        }
+    }
     for (let i = 0; i < telInputs.length; i++) {
         telInputs[i].addEventListener('input', onPhoneInput);
+        telInputs[i].addEventListener('keydown', onPhoneKeyDown);
+        telInputs[i].addEventListener('paste', onPhonePaste);
     }
 }
